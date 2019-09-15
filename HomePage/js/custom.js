6c6acaa7d67e721f77424f177a1cbebf925effed
Kiennguyen97 <vankiennguyen188@gmail.com>
2019-09-16 01:51:24 +0700

    update 16/09/2019

--- HomePage/js/custom.js
+++ HomePage/js/custom.js
@@ -2,14 +2,14 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (currentScrollPos < 10) {
-  //   $('#header')
-  // .delay(400)
-  // .queue(function (next) { 
-  //   $(this).css('height', '134px'); 
-  //   $(this).css('background', 'transparent'); 
+    //   $('#header')
+    // .delay(400)
+    // .queue(function (next) { 
+    //   $(this).css('height', '134px'); 
+    //   $(this).css('background', 'transparent'); 
 
-  //   next(); 
-  //   });
+    //   next(); 
+    //   });
     document.getElementById("header").style.height = "134px";
     document.getElementById("header").style.background = "transparent";
   } else {
@@ -38,20 +38,20 @@ $(document).ready(function () {
     paginationSpeed: 400,
     singleItem: true,
     items: 1,
-    
+
 
   });
-  owl.on('changed.owl.carousel', function(e) {
+  owl.on('changed.owl.carousel', function (e) {
     restart();
   });
 });
-  
+
 
 $(document).ready(restart());
-function restart(){
-  $('.item-extend').css({'right': '-1px'}),
-  $('.item-extend').css({'display': 'none'}),
-  $('.item-extend').animate({'right': '19.4vw'}, {"duration":2000,"queue":false});
+function restart() {
+  $('.item-extend').css({ 'right': '-1px' }),
+    $('.item-extend').css({ 'display': 'none' }),
+    $('.item-extend').animate({ 'right': '19.4vw' }, { "duration": 2000, "queue": false });
   $('.item-extend').fadeIn(2000);
 }
 
@@ -59,23 +59,21 @@ $(document).ready(function () {
 
   owl = $(".slider-bestsell").owlCarousel({
     navigation: true,
-    
+
     slideSpeed: 300,
     paginationSpeed: 400,
     items: 1,
-    responsive:{
-      500:{
-          items:2,
+    responsive: {
+      
+      600: {
+        items: 3,
       },
-      900:{
-          items:3,
-      },
-      1100:{
-          items:4,
+      1100: {
+        items: 4,
       }
     }
   });
-  
+
 });
 
 //TODO: Navbar-nav
@@ -106,3 +104,20 @@ function showList(evt, name) {
   document.getElementById(name).style.display = "block";
   evt.currentTarget.className += " active";
 }
+//list-footer
+//TODO: bật tắt class
+$("span.badge-submit").click(function () {
+  var right = $("input.email-submit").css("right");
+  // $("input.email").toggleClass("col-xl-9");
+  // if($("span.sub").hasClass("offset-3")){
+  //   $("input.email").removeClass("col-xl-6");
+  //   $("input.email").addClass("col-xl-9");
+  //   $("span.sub").removeClass("offset-3");
+  // }else{
+  //   $("input.email").addClass("col-xl-6");
+  //   $("input.email").removeClass("col-xl-9");
+  //   $("span.sub").addClass("offset-3");
+  // }
+  $("input.email-submit").toggleClass("open");
+  $(this).toggleClass("rotate");
+});
